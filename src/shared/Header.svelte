9a4c36a11cd8f6bbd32ce1fb2ad61ef8357b0b62
Hüzeyfe TAŞ <huzeyfetas@outlook.com>
2022-03-11 06:12:19 +0300

    header added

---
+++ src/shared/Header.svelte
@@ -0,0 +1,30 @@
+<header>
+  <h1>MeetUps</h1>
+  <nav>
+    <ul>
+      <li>Link 1</li>
+      <li>link 2</li>
+      <li>link 3</li>
+    </ul>
+  </nav>
+</header>
+
+<style>
+  header {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 4rem;
+    background-color: #cf0056;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    box-shadow: 0 2px 6px rgba(0, 0, 0, 0.2);
+  }
+  h1 {
+    margin: 0;
+    color: white;
+    font-family: "Roboto Slab", serif;
+  }
+</style>
